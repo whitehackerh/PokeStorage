59fe1fdb00681320a289af90a0fc8141c2bb571b
whitehackerh <hibiki.takahashi0801@gmail.com>
2024-08-30 01:31:40 +0900

    fix ui

--- frontend/src/components/pages/menu/Menu.tsx
+++ frontend/src/components/pages/menu/Menu.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { Container, Typography, List, ListItem } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import { Title } from '../../../entity/title';
-import './Menu.css';
 
 const Menu = () => {
     const location = useLocation();
@@ -25,22 +24,22 @@ const Menu = () => {
                     {title.title}
                 </Typography>
                 <List>
-                    <ListItem className='menuItem'>
+                    <ListItem style={{ justifyContent: 'center', backgroundColor: '#f0f0f0', margin: '10px 0', borderRadius: '8px' }}>
                         <Typography variant="body1" style={{ fontSize: '1.5rem' }}>
                             Registered Pokémon List
                         </Typography>
                     </ListItem>
-                    <ListItem className='menuItem'>
+                    <ListItem style={{ justifyContent: 'center', backgroundColor: '#f0f0f0', margin: '10px 0', borderRadius: '8px' }}>
                         <Typography variant="body1" style={{ fontSize: '1.5rem' }}>
                             Register Pokémon
                         </Typography>
                     </ListItem>
-                    <ListItem className='menuItem'>
+                    <ListItem style={{ justifyContent: 'center', backgroundColor: '#f0f0f0', margin: '10px 0', borderRadius: '8px' }}>
                         <Typography variant="body1" style={{ fontSize: '1.5rem' }}>
                             Team List
                         </Typography>
                     </ListItem>
-                    <ListItem className='menuItem'>
+                    <ListItem style={{ justifyContent: 'center', backgroundColor: '#f0f0f0', margin: '10px 0', borderRadius: '8px' }}>
                         <Typography variant="body1" style={{ fontSize: '1.5rem' }}>
                             Register Team
                         </Typography>
