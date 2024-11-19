4a78ffcd3892e05f2d58f38e06445c91a31992c9
whitehackerh <hibiki.takahashi0801@gmail.com>
2024-11-19 19:25:39 +0900

    remove dead code

--- frontend/src/components/pages/register_pokemon/RegisterPokemon.tsx
+++ frontend/src/components/pages/register_pokemon/RegisterPokemon.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { TextField, Box, Button } from '@mui/material';
 import { getItems } from '../../../api/Items';
