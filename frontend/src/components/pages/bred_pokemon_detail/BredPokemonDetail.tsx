b5cce0c6d1ae6e3ec905857961f800f9317ac0dd
whitehackerh <hibiki.takahashi0801@gmail.com>
2024-11-22 01:17:17 +0900

    list page

---
+++ frontend/src/components/pages/bred_pokemon_detail/BredPokemonDetail.tsx
@@ -0,0 +1,7 @@
+const BredPokemonDetail = () => {
+    return (
+        <></>
+    )
+}
+
+export default BredPokemonDetail;
--- frontend/src/components/pages/bred_pokemon_list/BredPokemonList.tsx
+++ frontend/src/components/pages/bred_pokemon_list/BredPokemonList.tsx
@@ -1,20 +1,18 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Title } from '../../../entity/Title';
-import { TitleEnum } from '../../../enum/Title';
 import { SVBredPokemon } from '../../../entity/SVBredPokemon';
 import { getBredPokemons } from '../../../api/BredPokemons';
+import "./BredPokemonListPage.css";
 
 const BredPokemonListPage = () => {
     const location = useLocation();
     const [title, setTitle] = useState<Title | null>(null);
-    const [toggleTeraType, setToggleTeraType] = useState<boolean>(false);
     const [bredPokemons, setBredPokemons] = useState<SVBredPokemon[]>([]);
 
     useEffect(() => {
         if (location.state && location.state.title) {
             setTitle(location.state.title);
-            setToggleTeraType(location.state.title.id == TitleEnum.SV);
         }
     }, [location.state]);
 
@@ -31,13 +29,44 @@ const BredPokemonListPage = () => {
         }
     }, [title]);
 
-    if (!title) {
+    const getTypeIconPath = (typeName: string) => {
+        try {
+            return require(`../../../assets/img/type/${typeName.toLowerCase()}.png`);
+        } catch {
+            return "";
+        }
+    };
+
+    if (!title || !bredPokemons) {
         return <div>Loading...</div>;
     }
 
     return (
-        <></>
-    )
+        <div className="pokemon-list">
+            {bredPokemons.map((pokemon) => (
+                <div key={pokemon.bredPokemon.id} className="pokemon-card">
+                    <div className="pokemon-header">
+                        <h3>{pokemon.bredPokemon.name} {pokemon.bredPokemon.formeName && (
+                        `(${pokemon.bredPokemon.formeName})`
+                        )}</h3>
+                    </div>
+                    {pokemon.bredPokemon.heldItem && (
+                            <p className="held-item">@{pokemon.bredPokemon.heldItem.name}</p>
+                        )}
+                    <p>{pokemon.bredPokemon.nature.name}</p>
+                    <p>{pokemon.bredPokemon.ability.name}</p>
+                    <div className="moves">
+                        <ul>
+                            {pokemon.bredPokemon.moves.map((move) => (
+                                <li key={move.name}>{move.name}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <p className="note">{pokemon.bredPokemon.note}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default BredPokemonListPage;
