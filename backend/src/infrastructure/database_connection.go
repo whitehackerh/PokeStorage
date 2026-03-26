bdfdb8dff9446e58028abb98f13b34f2729d907c
whitehackerh <hibiki.takahashi0801@gmail.com>
2026-03-26 09:56:15 +0900

    fix

--- backend/src/infrastructure/database_connection.go
+++ backend/src/infrastructure/database_connection.go
@@ -1,16 +1,35 @@
 package infrastructure
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDb() *gorm.DB {
 	var err error
-	dsn := "host=db user=postgres password=postgres dbname=pokestorage port=5432 sslmode=disable TimeZone=Asia/Tokyo"
+
+	user := getEnv("DB_USER", "")
+	password := getEnv("DB_PASSWORD", "")
+	dbname := getEnv("DB_NAME", "")
+
+	dsn := fmt.Sprintf(
+		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Tokyo",
+		user, password, dbname,
+	)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 	return db
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
 }
